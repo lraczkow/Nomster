4fd9114a6ba931c35a67a5785cb23e8d926e595c
Lauren Raczkowsk <lracz117@gmail.com>
2019-06-17 18:07:47 +0000

    place form and submission

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,4 +4,18 @@ class PlacesController < ApplicationController
     @places = Place.order("name").paginate(page: params[:page]).per_page(5)
   end
 
+  def new
+    @place = Place.new
+  end
+
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
 end
