25865363aac082079f66099b838762eb284914bb
Lauren Raczkowsk <lracz117@gmail.com>
2019-06-19 22:47:33 +0000

    geocoder added

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,8 @@
 class Place < ApplicationRecord
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode
+  
   validates :name, length: { minimum: 3 }
   validates :address, presence: true
   validates :description, presence: true
