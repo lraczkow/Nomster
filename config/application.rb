f2821f22a5dd43695ae23833d1e39e51e9bbf9a1
Lauren Raczkowsk <lracz117@gmail.com>
2019-06-20 18:00:19 +0000

    maps and places

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,6 @@ module Nomster
 
     # Precompile additional assets
     config.assets.precompile += %w( .svg .eot .woff .ttf )
-
-    config.assets.initialize_on_precompile = false
-    
+ 
   end
 end
