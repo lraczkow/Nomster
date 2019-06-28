3a3fb0f2a10fc2ae080b0d24af6658f7933d8a82
Lauren Raczkowsk <lracz117@gmail.com>
2019-06-28 21:54:09 +0000

    completed image uploader

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_28_151924) do
+ActiveRecord::Schema.define(version: 2019_06_28_204133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_06_28_151924) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "picture"
+    t.integer "user_id"
   end
 
   create_table "places", force: :cascade do |t|
