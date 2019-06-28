c39080bee9b76a7d679cd26ae600076fabfed98c
Lauren Raczkowsk <lracz117@gmail.com>
2019-06-28 19:48:36 +0000

    photo update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,7 @@ class CommentsController < ApplicationController
   def create
     @place = Place.find(params[:place_id])
     @place.comments.create(comment_params.merge(user: current_user))
+
     redirect_to place_path(@place)
   end
 
