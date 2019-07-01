e085872fdf94e2ef19a238bde90215c145d7855e
Lauren Raczkowsk <lracz117@gmail.com>
2019-07-01 22:22:09 +0000

    user dashboard page

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
     resources :comments, only: :create
     resources :photos, only: :create
   end
+  resources :users, only: :show
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
